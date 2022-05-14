46b4ddf96f0a056c5568ad9fa433fe0e15e7bef6
jose <joseluis836@hotmail.com>
2022-05-14 12:04:10 -0500

    Se agregaron los estilos globales y se definieron los colores base de la web

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,31 @@
-
-//Desde gulp llamaremos los métodos src y dest
+//src:source, dest: destination, watch
 const{src, dest, watch} = require('gulp');
 //Requiere el puente entre gulp-sass y la libreria de sass
 const sass = require('gulp-sass')(require('sass'));
+//Instanciar plumber evita el cierre de ejecución por errores
+const plumber = require('gulp-plumber');
 
 //Función para compilar scss a salida css
 function css(done){
     //Identificar la cartpeta de orignen de estilos
-    src("src/scss/app.scss")
+    src("src/scss/**/*.scss")
+    // Evitar el cierre por errores
+        .pipe(plumber())
     //Compilarla.. pipe para una tarea secuencial
         .pipe(sass())
     //Almacenar la salida en estilos con extensión css 
         .pipe(dest("build/css"))
+   
     done();
 }
-
-exports.css = css;
 
 //Función para agregar cambios automaticos
 function dev(done){
     //Requiere un archivo a estar atento a cambios y la función
     //que se ejecuta. Similar a un evento.
-    watch('src/scss/app.scss',css);
+    watch("src/scss/**/*.scss",css);
     done();
 }
+
+exports.css = css;
 exports.dev = dev;
